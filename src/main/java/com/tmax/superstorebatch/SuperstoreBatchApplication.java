50a43dcf20fe9c82790e7835ab97d900b7b30730
jjeong-ryu <jeongju_ryu@tmax.co.kr>
2023-06-01 14:39:01 +0900

    Initial commit

---
+++ src/main/java/com/tmax/superstorebatch/SuperstoreBatchApplication.java
@@ -0,0 +1,13 @@
+package com.tmax.superstorebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperstoreBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperstoreBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tmax/superstorebatch/SuperstoreBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmax.superstorebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperstoreBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
